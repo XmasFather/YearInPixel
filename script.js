7bef4430113ead3f65442112507a2398763f0a8d
XmasFather <xcraft.father@gmail.com>
2020-01-07 23:36:32 +0100

    animation logo

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function menuUtilisateur(){
+/* Affichage du menu quand on clique sur l'avatar */
+
+function affichageMenuUtilisateur(){
     var menuUtilisateur = document.querySelector(".menu-utilisateur");
     var affichage = menuUtilisateur.classList;
 
-    if(affichage == "hide"){
+    if(affichage == "menu-utilisateur hide"){
         menuUtilisateur.classList.remove("hide");
     }
     else{
@@ -10,4 +12,33 @@ function menuUtilisateur(){
     }
 }
 
-document.querySelector(".infos-utilisateur img").addEventListener('click', menuUtilisateur);
+document.querySelector(".infos-utilisateur img").addEventListener('click', affichageMenuUtilisateur);
+
+/* Changement de l'affichage mois à année */
+
+function swap(){
+    var nomJours = document.getElementById("nom-jours");
+    var nomMois = document.getElementById("nom-mois");
+    var numeroJours = document.getElementById("numero-jours");
+    var listeJours = document.getElementById("liste-jours");
+    var affichage = listeJours.classList;
+
+    if(affichage=="mois"){
+      affichage = "annee";
+      nomJours.classList.add("hide");
+      nomMois.classList.remove("hide");
+      numeroJours.classList.remove("hide");
+      listeJours.classList.remove("mois");
+      listeJours.classList.add("annee");
+    }else{
+      affichage = "mois";
+      nomJours.classList.remove("hide");
+      nomMois.classList.add("hide");
+      numeroJours.classList.add("hide");
+      listeJours.classList.remove("annee");
+      listeJours.classList.add("mois");
+    }
+
+  }
+
+  document.querySelector("h2").addEventListener('click', swap);  
--- test-connexion.php
+++ test-connexion.php
@@ -15,53 +15,221 @@
         /*header('location:connexion.html');
         exit();*/
     }
+    /* */
     $utilisateur = $bdd->query("SELECT * FROM utilisateur WHERE id = $valeurUtilisateur");
+    /* Change l'état de $utilisateur pour le rendre manipulable */
     $utilisateur = $utilisateur->fetch();
     
 
 ?>
-<?php 
-        include('head.php');
+<?php   
+    /* On appelle le head qui gère toutes infos relative à la page */
+    include('head.php');
 ?>
-<!-- Récupération des couleurs personnalisées en fonction des humeurs de l'utilisateur  -->
 <style>
     <?php 
+        /* Récupération des couleurs personnalisées en fonction des humeurs de l'utilisateur */
         miseEnPlaceCouleur($bdd, $utilisateur[id]);
     ?>
 </style>
 <body>
-    <!-- On appelle le header qui sera commun à toutes nos pages  -->
     <?php 
+        /* On appelle le header qui sera commun à toutes nos pages */
         include('header.php');
     ?>
     <main>
-        <!-- Affichage des humeurs personnalisées de chaque utilisateur  -->
+        <header class="header-main">
+        <nav role="navigation">
+            
+        </nav>
+        <h2>Mois <--> Année</h2>
+        </header>
+        <section class="calendrier">
+        <ul id="nom-jours" class="">
+            <li>L</li>
+            <li>M</li>
+            <li>M</li>
+            <li>J</li>
+            <li>V</li>
+            <li>S</li>
+            <li>D</li>
+        </ul>
+        
+        <ul id="nom-mois" class="hide">
+            <li>J</li>
+            <li>F</li>
+            <li>M</li>
+            <li>A</li>
+            <li>M</li>
+            <li>J</li>
+            <li>J</li>
+            <li>A</li>
+            <li>S</li>
+            <li>O</li>
+            <li>N</li>
+            <li>D</li>
+        </ul>
+        
+        <ul id="numero-jours" class="hide">
+            <li>rien</li>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li>
+            <li>8</li>
+            <li>9</li>
+            <li>10</li>
+            <li>11</li>
+            <li>12</li>
+            <li>13</li>
+            <li>14</li>
+            <li>15</li>
+            <li>16</li>
+            <li>17</li>
+            <li>18</li>
+            <li>19</li>
+            <li>20</li>
+            <li>21</li>
+            <li>22</li>
+            <li>23</li>
+            <li>24</li>
+            <li>25</li>
+            <li>26</li>
+            <li>27</li>
+            <li>28</li>
+            <li>29</li>
+            <li>30</li>
+            <li>31</li>
+        </ul>
+        
+        <ul id="liste-jours" class="mois">
+            <li>
+            <ul class="modif-humeur">
+                <li><div class="couleur-humeur-1"></div><p>Heureux</p></li>
+                <li><div class="couleur-humeur-2"></div> <p>Excité</p></li>
+                <li><div class="couleur-humeur-3"></div><p>Normal</p></li>
+                <li><div class="couleur-humeur-4"></div><p>Enervé</p></li>
+                <li><div class="couleur-humeur-5"></div><p>Stressé</p></li>
+                <li><div class="couleur-humeur-6"></div><p>Triste</p></li>
+                </ul>
+            </li>
+            <li>
+            <ul class="modif-humeur">
+                <li><div class="couleur-humeur-1"></div><p>Heureux</p></li>
+                <li><div class="couleur-humeur-2"></div> <p>Excité</p></li>
+                <li><div class="couleur-humeur-3"></div><p>Normal</p></li>
+                <li><div class="couleur-humeur-4"></div><p>Enervé</p></li>
+                <li><div class="couleur-humeur-5"></div><p>Stressé</p></li>
+                <li><div class="couleur-humeur-6"></div><p>Triste</p></li>
+            </ul>
+            </li>
+            <li>
+            <ul class="modif-humeur">
+                <li><div class="couleur-humeur-1"></div><p>Heureux</p></li>
+                <li><div class="couleur-humeur-2"></div> <p>Excité</p></li>
+                <li><div class="couleur-humeur-3"></div><p>Normal</p></li>
+                <li><div class="couleur-humeur-4"></div><p>Enervé</p></li>
+                <li><div class="couleur-humeur-5"></div><p>Stressé</p></li>
+                <li><div class="couleur-humeur-6"></div><p>Triste</p></li>
+            </ul>
+            </li>
+            <li>
+            <ul class="modif-humeur">
+                <li><div class="couleur-humeur-1"></div><p>Heureux</p></li>
+                <li><div class="couleur-humeur-2"></div> <p>Excité</p></li>
+                <li><div class="couleur-humeur-3"></div><p>Normal</p></li>
+                <li><div class="couleur-humeur-4"></div><p>Enervé</p></li>
+                <li><div class="couleur-humeur-5"></div><p>Stressé</p></li>
+                <li><div class="couleur-humeur-6"></div><p>Triste</p></li>
+            </ul>
+            </li>
+            <li>
+            <ul class="modif-humeur">
+                <li><div class="couleur-humeur-1"></div><p>Heureux</p></li>
+                <li><div class="couleur-humeur-2"></div> <p>Excité</p></li>
+                <li><div class="couleur-humeur-3"></div><p>Normal</p></li>
+                <li><div class="couleur-humeur-4"></div><p>Enervé</p></li>
+                <li><div class="couleur-humeur-5"></div><p>Stressé</p></li>
+                <li><div class="couleur-humeur-6"></div><p>Triste</p></li>
+            </ul>
+            </li>
+            <li>
+            <ul class="modif-humeur">
+                <li><div class="couleur-humeur-1"></div><p>Heureux</p></li>
+                <li><div class="couleur-humeur-2"></div> <p>Excité</p></li>
+                <li><div class="couleur-humeur-3"></div><p>Normal</p></li>
+                <li><div class="couleur-humeur-4"></div><p>Enervé</p></li>
+                <li><div class="couleur-humeur-5"></div><p>Stressé</p></li>
+                <li><div class="couleur-humeur-6"></div><p>Triste</p></li>
+            </ul>
+            </li>
+            <li>
+            <ul class="modif-humeur">
+                <li><div class="couleur-humeur-1"></div><p>Heureux</p></li>
+                <li><div class="couleur-humeur-2"></div> <p>Excité</p></li>
+                <li><div class="couleur-humeur-3"></div><p>Normal</p></li>
+                <li><div class="couleur-humeur-4"></div><p>Enervé</p></li>
+                <li><div class="couleur-humeur-5"></div><p>Stressé</p></li>
+                <li><div class="couleur-humeur-6"></div><p>Triste</p></li>
+            </ul>
+            </li>
+            <li>
+            <ul class="modif-humeur">
+                <li><div class="couleur-humeur-1"></div><p>Heureux</p></li>
+                <li><div class="couleur-humeur-2"></div> <p>Excité</p></li>
+                <li><div class="couleur-humeur-3"></div><p>Normal</p></li>
+                <li><div class="couleur-humeur-4"></div><p>Enervé</p></li>
+                <li><div class="couleur-humeur-5"></div><p>Stressé</p></li>
+                <li><div class="couleur-humeur-6"></div><p>Triste</p></li>
+            </ul>
+            </li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        
+        </section>
         <section class="humeur">
             <?php 
+                /* Affichage des humeurs personnalisées de chaque utilisateur */
                 affichageHumeur($bdd, $utilisateur[id]); 
             ?>
         </section>
     </main>
-    <!-- On appelle le footer qui sera commun à toutes nos pages  -->
+    
     <?php 
+        /* On appelle le footer qui sera commun à toutes nos pages */
         include('footer.php');
     ?>
 
-    <script>
-        function affichageMenuUtilisateur(){
-            var menuUtilisateur = document.querySelector(".menu-utilisateur");
-            var affichage = menuUtilisateur.classList;
-
-            if(affichage == "menu-utilisateur hide"){
-                menuUtilisateur.classList.remove("hide");
-            }
-            else{
-                menuUtilisateur.classList.add("hide");
-            }
-        }
-
-        document.querySelector(".infos-utilisateur img").addEventListener('click', affichageMenuUtilisateur);
-    </script>
+<script type="text/javascript" src="script.js"></script>
     
 </body>
 </html>
