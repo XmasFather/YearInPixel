a1111a7735717e2fcc0d2337ebfead6d5ffcce46
XmasFather <xcraft.father@gmail.com>
2020-06-08 20:52:45 +0200

    récupération des couleurs pour le profil de chaque utilisateur

--- functions.php
+++ functions.php
@@ -1,17 +1,5 @@
 <?php
-    /* Permet de récupérer les couleurs personnalisées de chacun */
-    function miseEnPlaceCouleur($bdd, $idUtilisateur){
-        $couleurs = $bdd->query("SELECT * FROM humeur WHERE utilisateur_id = $idUtilisateur");
-        $compteur = 0;
-        echo ":root{ \n     --color-background: #fff8ed; \n     --color-text: #333333; \n     --color-0: #ffffff; \n";
-        foreach($couleurs as $humeur){
-            $compteur ++;
-            echo "     --color-".$compteur.": #".$humeur[couleur]."; \n";
-        }
-        echo "} \n";
-    }
-
-    function affichageModificationHumeur($bdd, $idUtilisateur){
+        function affichageModificationHumeur($bdd, $idUtilisateur){
         $couleurs = $bdd->query("SELECT * FROM humeur WHERE utilisateur_id = $idUtilisateur");
         $compteur = 0;
         foreach($couleurs as $humeur){
